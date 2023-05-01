d349cf2966db325967c876885b89a6d7e908cdb2
atroopa <atroopa@gmail.com>
2023-05-01 17:42:17 +0330

    fetch api work true

--- pages/index.js
+++ pages/index.js
@@ -14,24 +14,31 @@ export default function Home({articles}) {
         <meta name="keyword" content="home , persian, iranian , parsclick, tutorial"/>
       </Head>
 
-      <div className='text-5xl'>
+      <div className='py-5 text-5xl'>
         Welcome to next
+      </div>
+
+      <div className='text-2xl'>
+          <ul>
+            {articles.map((article) => (
+              <li key={article.id}>{article.title}</li>
+            ))}
+        </ul>
       </div>
 
     </div>
   )
 }
 
-export const getStaticProps = async () => {
+export async function getStaticProps() {
 
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=6");
-  const articles = await response.json()
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+  const articles = await res.json()
+
 
   return {
     props: {
-      articles
-    }
+      articles,
+    },
   }
-
-
 }
