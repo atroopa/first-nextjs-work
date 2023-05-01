2f1c5583c14a2d0b23b77ff534389091894f4078
atroopa <atroopa@gmail.com>
2023-05-01 16:36:39 +0330

    navbar finish

--- components/Nav.js
+++ components/Nav.js
@@ -3,9 +3,7 @@ import Link from 'next/link'
 
 const Nav = () => {
   return (
-
-
-        <ul className='text-2xl flex py-8 px-8 bg-gray-700' >
+        <ul className='text-2xl flex py-8 px-8 bg-gray-900' >
             <li className="mr-6">
                 <Link className="text-gray-100 hover:text-gray-500" href="/">Home</Link>
             </li>
@@ -16,9 +14,6 @@ const Nav = () => {
                 <Link className="text-gray-100 hover:text-gray-500" href="/contact">Contact</Link>
             </li>
         </ul>        
-
-
-
   )
 }
 
