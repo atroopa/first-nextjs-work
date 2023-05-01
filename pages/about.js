bdd2f6fb514e76296194c3f4247a9837a45632b8
atroopa <atroopa@gmail.com>
2023-05-01 13:10:16 +0330

    about page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+
+export default function about() {
+    return (
+        <div>
+            <Head>
+                <title>ParsClick - About</title>
+                <meta name="keyword" content="about, persian, iranian , parsclick, tutorial"/>
+            </Head>
+            <div className="text-5xl font-bold">about</div>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Head from 'next/head' 
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -6,7 +6,15 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-      Home
+      <Head>
+        <title>Pars Click - Home</title>
+        <meta name="keyword" content="home , persian, iranian , parsclick, tutorial"/>
+      </Head>
+
+      <div className='text-5xl'>
+        Welcome to next
+      </div>
+
     </div>
   )
 }
