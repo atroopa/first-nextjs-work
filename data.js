5ac0ff540c33bb789d497ae8e10b889dd38cb741
atroopa <atroopa@gmail.com>
2023-05-02 17:17:46 +0330

    create my own api for test

---
+++ data.js
@@ -0,0 +1,18 @@
+export const articles = [
+    {
+        id: '1',
+        title: 'ParsClick introduces dark mode and auto-merg pull request',
+        exerpt: 
+        'keep up with th latest news and event from the world of web technology',
+        body:
+        'sunt aut facere repellat provident occaecati excepturi optio reprehenderit quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
+    },
+    {
+        id: '2',
+        title: 'ParsClick introduces  request',
+        exerpt: 
+        'keep up keep up with th latest news and event from the world of web technology th latest news and event from the world of web technology',
+        body:
+        'sunt qui est esse est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla facere repellat provident occaecati excepturi optio reprehenderit quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
+    }
+]
